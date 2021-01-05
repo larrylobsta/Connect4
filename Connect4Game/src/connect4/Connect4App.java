8a25cc4064272a6d4a5e6531f7c309499a21b70b
larrylobsta <76498261+larrylobsta@users.noreply.github.com>
2021-01-04 21:22:06 -0800

    add files via upload

---
+++ Connect4Game/src/connect4/Connect4App.java
@@ -0,0 +1,207 @@
+package connect4;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Connect4App extends Application {
+    final int COLUMNS = 7;
+    final int ROWS = 6;
+    final int cellSize = 100;
+    
+    Color yellow = Color.YELLOW;
+    Color red = Color.INDIANRED;
+    Color white = Color.WHITE;
+
+    boolean yellowTurn = true;
+    boolean gameOver = false;
+
+    Slot[][] grid = new Slot[COLUMNS][ROWS];
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane root = new BorderPane();
+        Pane gamePane = new Pane();
+        gamePane.getChildren().add(makeBoard());
+        gamePane.getChildren().addAll(makeSlots());
+        gamePane.getChildren().addAll(makeColumns());
+
+        root.setCenter(gamePane);
+        root.setRight(makeSidePane());
+
+        primaryStage.setTitle("Connect4");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public Shape makeBoard() {
+        Shape board = new Rectangle(COLUMNS * cellSize, ROWS * cellSize);
+        board.setFill(new Color(0.2,0.4,0.8,1));
+        return board;
+    }
+
+    public List<Slot> makeSlots() {
+        List<Slot> slots = new ArrayList<>();
+        for (int x = 0; x < COLUMNS; x++) {
+            for (int y = 0; y < ROWS; y++) {
+                Slot slot = new Slot();
+                slot.setCenterX(cellSize / 2);
+                slot.setCenterY(cellSize / 2);
+                slot.setTranslateX(x * cellSize);
+                slot.setTranslateY(y * cellSize);
+
+                slots.add(slot);
+                grid[x][y] = slot; // places disks on the disk grid
+            }
+        }
+        return slots;
+    }
+
+    public List<Rectangle> makeColumns() {
+        List<Rectangle> columns = new ArrayList<>();
+        for (int x = 0; x < COLUMNS; x++) {
+            Rectangle column = new Rectangle(cellSize, ROWS * cellSize);
+            column.setTranslateX(x * cellSize);
+            column.setFill(Color.TRANSPARENT);
+
+            final int columnNum = x;
+            column.setOnMouseClicked(e -> placeDisk(columnNum));
+            columns.add(column);
+        }
+        return columns;
+    }
+
+    public void placeDisk(int columnNum) {
+        if (!gameOver) {
+            for (int i = ROWS - 1; i >= 0; i--) {
+                if (grid[columnNum][i].isEmpty()) {
+                    grid[columnNum][i].changeState(yellowTurn ? 1 : 2);
+                    if (checkForWin(yellowTurn ? 1 : 2)) {
+                        gameEnd();
+                        gameOver = true;
+                    }
+                    yellowTurn = !yellowTurn;
+                    break;
+                }
+            }
+        }
+    }
+
+    public boolean checkForWin(int i) {
+        int count = 0;
+        //check horizontally
+        for (int x = 0; x < COLUMNS - 3; x++) {
+            // horizontal
+            for (int y = 0; y < ROWS; y++) {
+                if (grid[x][y].getState() == i && grid[x + 1][y].getState() == i && grid[x + 2][y].getState() == i && grid[x + 3][y].getState() == i) {
+                    highlightWin(grid[x][y],grid[x + 1][y],grid[x + 2][y],grid[x + 3][y] );
+                    return true;
+                }
+            }
+            // descending diagonal
+            for (int y = 3; y < ROWS; y++) {
+                if (grid[x][y].getState() == i && grid[x + 1][y - 1].getState() == i && grid[x + 2][y - 2].getState() == i && grid[x + 3][y - 3].getState() == i) {
+                    highlightWin(grid[x][y],grid[x + 1][y - 1],grid[x + 2][y - 2],grid[x + 3][y - 3]);
+                    return true;
+                }
+            }
+            // ascending diagonal
+            for (int y = 0; y < ROWS - 3; y++) {
+                if (grid[x][y].getState() == i && grid[x + 1][y + 1].getState() == i && grid[x + 2][y + 2].getState() == i && grid[x + 3][y + 3].getState() == i) {
+                    highlightWin(grid[x][y],grid[x + 1][y + 1],grid[x + 2][y + 2],grid[x + 3][y + 3]);
+                    return true;
+                }
+            }
+        }
+        //check vertically
+        for (int x = 0; x < COLUMNS; x++) {
+            for (int y = 0; y < ROWS - 3; y++) {
+                if (grid[x][y].getState() == i && grid[x][y + 1].getState() == i && grid[x][y + 2].getState() == i && grid[x][y + 3].getState() == i) {
+                    highlightWin(grid[x][y],grid[x][y + 1],grid[x][y + 2],grid[x][y + 3]);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void highlightWin(Slot s1, Slot s2, Slot s3, Slot s4) {
+        s1.setStroke(Color.LIGHTGREEN);
+        s1.setStrokeWidth(5);
+        s2.setStroke(Color.LIGHTGREEN);
+        s2.setStrokeWidth(5);
+        s3.setStroke(Color.LIGHTGREEN);
+        s3.setStrokeWidth(5);
+        s4.setStroke(Color.LIGHTGREEN);
+        s4.setStrokeWidth(5);
+    }
+
+    public void gameEnd() {
+        //
+    }
+
+    public void resetGame() {
+        for (int x = 0; x < COLUMNS; x++) {
+            for (int y = 0; y < ROWS; y++) {
+                grid[x][y].changeState(0);
+                grid[x][y].setStrokeWidth(0);
+            }
+        }
+        gameOver = false;
+    }
+
+    public Pane makeSidePane() {
+        Pane sidePane = new Pane();
+        Button newGame = new Button("New Game");
+        newGame.setOnAction(e -> resetGame());
+
+        sidePane.getChildren().add(newGame);
+
+        return sidePane;
+    }
+
+
+    private class Slot extends Circle {
+        int state; // 0 represents empty slot, 1 means occupied by yellow, 2 means occupied by red
+
+        public Slot() {
+            super(cellSize / 2.2);
+            state = 0;
+            setFill(white);
+        }
+
+        public void changeState(int i) {
+            state = i;
+            if (i == 0) {
+                setFill(white);
+            } else if (i == 1) {
+                setFill(yellow);
+            } else if (i == 2) {
+                setFill(red);
+            }
+        }
+
+        public boolean isEmpty() {
+            return state == 0;
+        }
+
+        public int getState() {
+            return state;
+        }
+    }
+}
